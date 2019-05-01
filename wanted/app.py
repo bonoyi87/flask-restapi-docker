e920289d6af2f8e37098c6be3d304a25ff693dc5
Hyeonah <hyeonah.bono.lee@gmail.com>
2019-05-01 16:24:23 +0900

    Add init flask app

---
+++ wanted/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def create_app(config_object='wanted.settings'):
+    """An application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
+
+    :param config_object: The configuration object to use.
+    """
+    app = Flask(__name__.split('.')[0])
+    app.config.from_object(config_object)
+
+    return app
+
